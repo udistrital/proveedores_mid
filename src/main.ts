a68c43cdf5be2e86adcf93ec0c0828b990f17356
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-07-23 14:05:11 -0500

    feat: Actualizacion Deps udistrital/argo_documentacion/issues/74

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Proveedores API MID')
+    .setDescription('API')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
