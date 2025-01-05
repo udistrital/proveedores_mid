82b2d1774cbe6d982453f6cb71c13f3be652b0c3
LauwuRi <lain.2ri@gmail.com>
2025-01-05 15:25:28 -0500

    feat: Implementación tipo_persona_id udistrital/argo_documentacion#292

--- src/contratistas/contratistas.service.ts
+++ src/contratistas/contratistas.service.ts
@@ -24,10 +24,11 @@ interface responseContratoData {
   };
 }
 
-interface responseTipoPersonaData{
-  tipo_persona_id: {
-    tipo_persona_id: TipoPersonaDto[];
-  }
+interface responseTipoPersonaData {
+  Data: TipoPersonaDto[];
+  Message: string;
+  Status: string;
+  Success: boolean;
 }
 
 @Injectable()
@@ -55,6 +56,11 @@ export class ContratistasService {
       const infoProveedor = data.proveedores.proveedor[0];
       let detalles: DetalleProveedorDto;
       const contratos = await this.obtenerContratoProveedor(infoProveedor.numero_documento);
+
+      const tipoPersonaId = await this.obtenerTipoPersonaId(infoProveedor.tipo_persona);
+      if (tipoPersonaId) {
+        infoProveedor.tipo_persona_id = tipoPersonaId;
+      }
 
       if (infoProveedor.tipo_persona == 'JURIDICA') {
         detalles = await this.obtenerDetalleProveedor(
@@ -119,14 +125,14 @@ export class ContratistasService {
         return null;
       }
 
-      return [data.contratos_personas.contrato_persona[0]]; 
+      return [data.contratos_personas.contrato_persona[0]];
 
     } catch (error) {
       return null;
     }
   }
 
-  async obtenerTipoPersonaId(tipo: string): Promise<TipoPersonaDto> {
+  async obtenerTipoPersonaId(tipo: string): Promise<number | null> {
     try {
       const endpoint: string = this.configService.get<string>(
         'ENDP_PARAMETROS_CRUD',
@@ -134,13 +140,21 @@ export class ContratistasService {
       const TipoParametroId: string = this.configService.get<string>(
         'TIPO_PARAMETRO_ID',
       );
-      const url = `${endpoint}/parametro?query=TipoParametroId:${TipoParametroId},Nombre:${tipo}&limit=0`;
-      
+
+      const tipoFormat = this.formatText(tipo);
+      const url = `${endpoint}/parametro?query=TipoParametroId:${TipoParametroId},Nombre:${tipoFormat}&limit=0`;
+
       const { data } = await axios.get<responseTipoPersonaData>(url);
 
-      return data.tipo_persona_id[0];
+      return data.Data[0].Id;
     } catch (error) {
       return null;
     }
+  }
+
+  formatText(text) {
+    if (!text) return '';
+    text = text.trim().toLowerCase();
+    return text.charAt(0).toUpperCase() + text.slice(1);
   }
 }
